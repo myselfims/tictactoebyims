c97e39a52e4ca79c44579a16e0f30c5601df83e7
myselfims <shaikhimran7585@gmail.com>
2022-09-06 18:38:14 +0530

    bug fix

--- tictac/settings.py
+++ tictac/settings.py
@@ -134,7 +134,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": os.environ.get('REDIS_URL'),
+            "hosts": os.environ.get('REDIS_URL','redis://localhost:6379'),
         },
     },
 }
